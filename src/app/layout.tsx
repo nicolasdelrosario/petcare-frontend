188cb22f4ab64b750d00944ef78ff83376f2dd18
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-09-30 10:57:39 -0500

    refactor: :sparkles: update styles in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ import { Recursive } from 'next/font/google'
 // CSS
 import './globals.css'
 
+// Shadcn
+import { cn } from '@/lib/utils'
+
+// Components
+import { Navbar } from '@/components'
+
 const recursive = Recursive({ subsets: ['latin'] })
 
 export const metadata = constructMetadata()
@@ -17,9 +23,11 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
-		<html lang='en'>
-			<body className={recursive.className}>
-				<header></header>
+		<html lang='es'>
+			<body className={cn('min-h-screen', recursive.className)}>
+				<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+					<Navbar />
+				</header>
 				{children}
 				<footer></footer>
 			</body>
