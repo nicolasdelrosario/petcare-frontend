c215804fa026c525a06e4fb65cd969f49f29ce22
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-01 00:32:23 -0500

    feat: :sparkles: add value-proposition-section at home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,19 @@ import { MaxWidthWrapper } from '@/components'
 import { buttonVariants } from '@/components/shadcn/button'
 
 // Icons
-import { ArrowRightIcon } from '@radix-ui/react-icons'
+import { ArrowRightIcon, CheckIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
+	const features = [
+		'Gestión centralizada de citas y clientes.',
+		'Automatización de recordatorios para citas.',
+		'Acceso fácil a reportes financieros y métricas clave.',
+		'Personalización de perfiles y roles de empleados.',
+		'Interfaz intuitiva y fácil de usar.',
+		'Seguridad avanzada para proteger la información de tus clientes y pacientes.',
+		'Soporte dedicado y atención personalizada.',
+	]
+
 	return (
 		<div className='bg-slate-50'>
 			{/* main section */}
@@ -45,6 +55,42 @@ export default function Home() {
 							alt='Petcare Home Image'
 							className='rounded-md'
 						/>
+					</div>
+				</MaxWidthWrapper>
+			</section>
+
+			{/* value proposition section */}
+			<section className='bg-slate-100 py-24'>
+				<MaxWidthWrapper className='flex flex-col items-center'>
+					<h2 className='mt-2 text-balance text-center text-4xl font-bold leading-relaxed tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>
+						Nuestros Beneficios
+					</h2>
+					<div className='pt-10 lg:grid lg:grid-cols-4 lg:gap-x-4 lg:pt-24 xl:gap-x-8 xl:pt-32'>
+						<div className='col-span-full lg:order-2 lg:col-span-2'>
+							<h3 className='mt-2 text-balance text-center text-2xl font-bold leading-relaxed tracking-tight text-gray-900 md:text-3xl lg:text-start lg:text-4xl'>
+								Descubre los Beneficios de PetCare
+							</h3>
+							{
+								<ul className='my-8 flex flex-col items-start space-y-2 text-left font-medium lg:mb-0'>
+									{features.map((feature, index) => (
+										<li
+											key={index}
+											className='flex items-center gap-2 text-left xl:text-lg'
+										>
+											<CheckIcon className='h-6 w-6 shrink-0 text-pretty text-primary' />
+											{feature}
+										</li>
+									))}
+								</ul>
+							}
+						</div>
+						<div className='col-span-full flex justify-center lg:col-span-2'>
+							<img
+								src='https://placehold.co/600x500'
+								alt='Petcare Benefits Image'
+								className='rounded-md'
+							/>
+						</div>
 					</div>
 				</MaxWidthWrapper>
 			</section>
