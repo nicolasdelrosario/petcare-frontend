a9ea0e20bc720e399e659ca8d480251fa50d3662
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-11 00:54:12 -0500

    feat: :art: export shadcn components file

---
+++ src/components/shadcn/index.ts
@@ -0,0 +1,11 @@
+export { Button, buttonVariants } from './button'
+export { Input } from './input'
+export {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from './table'
+export { Card, CardContent, CardHeader, CardTitle } from './card'
