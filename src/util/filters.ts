b12b6481e8ba4f2f01bf1cf6d46011c61ad0d87c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-11 01:08:12 -0500

    feat(dashboard): :sparkles: add filters

---
+++ src/util/filters.ts
@@ -0,0 +1,27 @@
+import { Appointment } from '@/interfaces/Appointment'
+import { Owner } from '@/interfaces/Owner'
+
+export function filterAppointments(
+	appointments: Appointment[],
+	searchTerm: string
+): Appointment[] {
+	const lowerCaseSearchTerm = searchTerm.toLowerCase()
+
+	return appointments.filter(
+		appointment =>
+			appointment.pet.owner.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+			appointment.pet.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+			appointment.date.toString().includes(lowerCaseSearchTerm)
+	)
+}
+
+export function filterOwners(owners: Owner[], searchTerm: string): Owner[] {
+	const lowerCaseSearchTerm = searchTerm.toLowerCase()
+
+	return owners.filter(
+		owner =>
+			(owner.name && owner.name.toLowerCase().includes(lowerCaseSearchTerm)) ||
+			(owner.dni && owner.dni.toLowerCase().includes(lowerCaseSearchTerm)) ||
+			(owner.email && owner.email.toLowerCase().includes(lowerCaseSearchTerm))
+	)
+}
