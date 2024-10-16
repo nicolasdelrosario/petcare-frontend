4ac2a8746099f6a77a5d7adcc054061b17c657f0
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-16 16:19:12 -0500

    feat(dashboard): :zap: add return interface in appointment.service methods

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,14 +1,18 @@
 import API_BASE from './api'
+
+// Interfaces
 import { Appointment } from '@/interfaces/Appointment'
 
 const PATH_APPOINTMENTS = '/appointments'
 
+// Get appointments
 export const getAppointments = async (): Promise<Appointment[]> => {
 	const { data } = await API_BASE.get(`${PATH_APPOINTMENTS}`)
 	return data
 }
 
-export const getAppointment = async (id: string): Promise<Appointment> => {
+// Get appointment
+export const getAppointment = async (id: number): Promise<Appointment> => {
 	const { data } = await API_BASE.get(`${PATH_APPOINTMENTS}/${id}`)
 	return data
 }
