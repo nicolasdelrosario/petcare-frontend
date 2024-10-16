8cac5fc39dfa074e57334f9786f2857caf0b88e3
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-16 16:17:49 -0500

    perf(global): :zap: add return interface in owner.service methods

--- src/services/owner.service.ts
+++ src/services/owner.service.ts
@@ -1,13 +1,18 @@
 import API_BASE from './api'
 
+// Interfaces
+import { Owner } from '@/interfaces/Owner'
+
 const PATH_OWNER = '/owners'
 
-export const getOwners = async () => {
+// Get Owners
+export const getOwners = async (): Promise<Owner[]> => {
 	const { data } = await API_BASE.get(`${PATH_OWNER}`)
 	return data
 }
 
-export const getOwner = async (id: string) => {
+// Get Owner
+export const getOwner = async (id: number): Promise<Owner> => {
 	const { data } = await API_BASE.get(`${PATH_OWNER}/${id}`)
 	return data
 }
