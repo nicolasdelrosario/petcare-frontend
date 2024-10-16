ce0aa14274cd4edf36bb9578ad03dd616c982de8
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-16 16:54:46 -0500

    feat(dashboard/appointments): :sparkles: add appointment details component

---
+++ src/app/dashboard/(pages)/citas/components/AppointmentDetails/index.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+// React
+import { useState } from 'react'
+
+// Zustand
+import { useAppointmentStore } from '@/store/useAppointmentStore'
+
+// Hooks
+import { useToast } from '@/hooks/useToast'
+
+// Interfaces
+import { Appointment } from '@/interfaces/Appointment'
+
+// Format Date
+import { format, parseISO, setHours, setMinutes } from 'date-fns'
+
+// Components
+import { EditableFields, AppointmentInfo } from '../index'
+import { ActionButtons } from '@/app/dashboard/components'
+
+interface AppointmentDetailsProps {
+	appointment: Appointment
+}
+
+export default function AppointmentDetails({
+	appointment,
+}: AppointmentDetailsProps) {
+	const { toast } = useToast()
+	const { updateAppointment, removeAppointment } = useAppointmentStore()
+	const [isEditing, setIsEditing] = useState(false)
+	const [time, setTime] = useState(format(new Date(appointment.date), 'HH:mm'))
+	const [editedAppointment, setEditedAppointment] = useState({
+		reason: appointment.reason,
+		date: format(new Date(appointment.date), 'yyyy-MM-dd'),
+	})
+
+	const updateEditedAppointment = (name: string, value: string) => {
+		if (name === 'time') {
+			setTime(value)
+		} else {
+			setEditedAppointment(prev => ({ ...prev, [name]: value }))
+		}
+	}
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		updateEditedAppointment(name, value)
+	}
+
+	const handleSave = async () => {
+		if (!editedAppointment.reason || !time) {
+			return toast({
+				title: 'Error',
+				description: 'Todos los campos son requeridos.',
+			})
+		}
+
+		const combinedDateTime = setMinutes(
+			setHours(parseISO(editedAppointment.date), parseInt(time.split(':')[0])),
+			parseInt(time.split(':')[1])
+		)
+
+		const updatedData = {
+			reason: editedAppointment.reason,
+			date: combinedDateTime,
+		}
+
+		const result = await updateAppointment(appointment.id, updatedData)
+		toast({
+			title: result.success ? 'Éxito' : 'Error',
+			description: result.message,
+		})
+		setIsEditing(false)
+	}
+
+	const handleDelete = async () => {
+		const result = await removeAppointment(appointment.id)
+		toast({
+			title: result.success ? 'Éxito' : 'Error',
+			description: result.message,
+		})
+	}
+
+	return (
+		<div className='pt-2'>
+			{isEditing ? (
+				<EditableFields
+					editedAppointment={editedAppointment}
+					time={time}
+					handleInputChange={handleInputChange}
+				/>
+			) : (
+				<AppointmentInfo appointment={appointment} />
+			)}
+			<ActionButtons
+				isEditing={isEditing}
+				handleSave={handleSave}
+				setIsEditing={setIsEditing}
+				handleDelete={handleDelete}
+			/>
+		</div>
+	)
+}
