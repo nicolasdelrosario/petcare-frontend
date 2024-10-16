50f159ef4c532fe3df919e8f8912afc642940e19
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-16 16:25:51 -0500

    feat(global): :sparkles: add export components file
    add export components file called index.ts where every shadcn component will be exported

--- src/components/shadcn/index.ts
+++ src/components/shadcn/index.ts
@@ -1,5 +1,8 @@
 export { Button, buttonVariants } from './button'
 export { Input } from './input'
+export { Label } from './label'
+export { Textarea } from './textarea'
+
 export {
 	Table,
 	TableBody,
@@ -8,4 +11,25 @@ export {
 	TableHeader,
 	TableRow,
 } from './table'
-export { Card, CardContent, CardHeader, CardTitle } from './card'
+
+export { Card, CardContent, CardHeader, CardTitle, CardFooter } from './card'
+
+export {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from './dialog'
+
+export {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from './alert-dialog'
