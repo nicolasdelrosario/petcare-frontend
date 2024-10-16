05097bbdc3c9e4f27227b9db03621a2f7c78d111
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-16 16:27:01 -0500

    feat(dashboard): :sparkles: add dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Sidebar from './components/Sidebar'
+import { cn } from '@/lib/utils'
+import { useSidebarStore } from '@/store/useSidebarStore'
+
+interface RootLayoutProps {
+	children: React.ReactNode
+}
+
+export default function Layout({ children }: RootLayoutProps) {
+	const { open, compact } = useSidebarStore()
+
+	return (
+		<div className='flex min-h-screen'>
+			<Sidebar />
+			<div
+				className={cn(
+					'flex flex-1 flex-col transition-all duration-300',
+					open ? (compact ? 'ml-16' : 'ml-64') : 'ml-16'
+				)}
+			>
+				{children}
+			</div>
+		</div>
+	)
+}
