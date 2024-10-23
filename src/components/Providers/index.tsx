b9e236137a908a6a75a75f6dfaf02af4e2d9a77c
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-23 11:57:48 -0500

    feat(global): :sparkles: add query-client-provider

---
+++ src/components/Providers/index.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+interface ProvidersProps {
+	children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				retry: 2,
+				refetchOnWindowFocus: false,
+			},
+		},
+	})
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			{process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+		</QueryClientProvider>
+	)
+}
