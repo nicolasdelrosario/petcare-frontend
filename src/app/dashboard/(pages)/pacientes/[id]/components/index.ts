888c7dbfc7f480a4e0a397aae723e0e507a229ce
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-27 20:07:01 -0500

    chore(dashboard/patients): :zap: add exportable file

---
+++ src/app/dashboard/(pages)/pacientes/[id]/components/index.ts
@@ -0,0 +1,13 @@
+// Owner
+export { default as Owner } from './Owner'
+export { default as OwnerDelete } from './OwnerDelete'
+export { default as OwnerUpdate } from './OwnerUpdate'
+export { default as OwnerDetails } from './OwnerDetails'
+
+// Pet
+export { default as Pet } from './Pet'
+export { default as PetCard } from './PetCard'
+export { default as PetDetails } from './PetDetails'
+export { default as PetDetailsDialog } from './PetDetailsDialog'
+export { default as PetEditableFields } from './PetEditableFields'
+export { default as PetGrid } from './PetGrid'
