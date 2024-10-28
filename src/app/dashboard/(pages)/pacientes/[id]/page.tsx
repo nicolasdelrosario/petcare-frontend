b0fc1ef0eb18ab94edaaa6e560c17f8fb230c380
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-27 20:07:36 -0500

    feat(dashboard/appointments): :sparkles: add selected patient

---
+++ src/app/dashboard/(pages)/pacientes/[id]/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+// Services
+import { useOwnerById } from '@/hooks/owners/useOwnerById'
+
+// Components
+import { MaxWidthWrapper } from '@/components'
+import { Header } from '@/app/dashboard/components'
+import { OwnerDetails, PetGrid } from './components'
+
+interface Props {
+	params: {
+		id: string
+	}
+}
+
+export default function Page({ params }: Props) {
+	const { data: owner, error, isLoading } = useOwnerById(parseInt(params.id))
+
+	if (isLoading) {
+		return <div>loading</div>
+	}
+
+	if (error || !owner) {
+		return <div></div>
+	}
+
+	const pets = owner?.pets || []
+
+	return (
+		<>
+			<Header title='Detalles del Propietario' />
+			<MaxWidthWrapper>
+				<div className='lg:grid lg:grid-cols-2 lg:gap-x-4 lg:pt-24 xl:gap-x-8 xl:pt-32'>
+					<PetGrid pets={pets} />
+					<OwnerDetails owner={owner} />
+				</div>
+			</MaxWidthWrapper>
+		</>
+	)
+}
