60c5cf3e22e19978a0f0bf0b50bdff696a6493cb
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-27 20:05:43 -0500

    feat(dashboard/patients): :sparkles: add owner details component

---
+++ src/app/dashboard/(pages)/pacientes/[id]/components/OwnerDetails/index.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+// React
+import { useState } from 'react'
+
+// Interfaces
+import { Owner as IOwner } from '@/interfaces/Owner'
+
+// Hooks
+import { useUpdateOwner } from '@/hooks/owners/useUpdateOwner'
+import { useDeleteOwner } from '@/hooks/owners/useDeleteOwner'
+
+// Components
+import { Owner, OwnerUpdate, OwnerDelete } from '../'
+
+// Shadcn Components
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/shadcn'
+
+interface OwnerDetailsProps {
+	owner: IOwner
+}
+
+export default function OwnerDetails({ owner }: OwnerDetailsProps) {
+	const [isEditing, setIsEditing] = useState(false)
+	const [editedOwner, setEditedOwner] = useState<IOwner>(owner)
+
+	const updateOwnerMutation = useUpdateOwner()
+	const deleteOwnerMutation = useDeleteOwner(owner.id)
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target
+		setEditedOwner(prev => ({
+			...prev,
+			[name]: value,
+		}))
+	}
+
+	const handleUpdate = () => {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { id, createdAt, updatedAt, deletedAt, pets, ...changes } =
+			editedOwner
+		updateOwnerMutation.mutate({
+			id: owner.id,
+			changes,
+		})
+
+		setIsEditing(false)
+	}
+
+	const handleDelete = () => deleteOwnerMutation.mutate()
+
+	return (
+		<section className='mt-8 lg:mt-4'>
+			<h2 className='mb-4 text-lg font-semibold'>Detalles del Propietario</h2>
+			<Card className='border'>
+				<CardHeader className='border-b p-4'>
+					<CardTitle className='text-md font-semibold'>
+						{owner.name || 'Nombre no especificado.'}
+					</CardTitle>
+				</CardHeader>
+				<CardContent className='space-y-3 p-4'>
+					<Owner owner={owner} />
+				</CardContent>
+				<CardFooter className='flex flex-col gap-y-3 px-4 pb-4'>
+					<OwnerUpdate
+						isEditing={isEditing}
+						setIsEditing={setIsEditing}
+						handleUpdate={handleUpdate}
+						handleInputChange={handleInputChange}
+						owner={editedOwner}
+					/>
+					<OwnerDelete handleDelete={handleDelete} />
+				</CardFooter>
+			</Card>
+		</section>
+	)
+}
