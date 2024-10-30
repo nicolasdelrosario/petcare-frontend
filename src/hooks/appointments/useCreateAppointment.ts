f6fa1fd3d2417937c2c5fb310fd8af401eb29e63
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-10-29 21:44:40 -0500

    feat(global): :sparkles: add useCreateAppointment hook

---
+++ src/hooks/appointments/useCreateAppointment.ts
@@ -0,0 +1,35 @@
+// Services
+import { createAppointment } from '@/services/appointment.service'
+
+// Interfaces
+import { Appointment } from '@/interfaces/Appointment'
+
+// Hooks
+import { useToast } from '@/hooks/useToast'
+
+// Tankstack Query
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+export const useCreateAppointment = () => {
+	const queryClient = useQueryClient()
+	const { toast } = useToast()
+
+	return useMutation({
+		mutationFn: (changes: Partial<Appointment>) => createAppointment(changes),
+
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['appointments'] })
+			toast({
+				title: 'Éxito',
+				description: 'La cita ha sido creada exitosamente.',
+			})
+		},
+
+		onError: () => {
+			toast({
+				title: 'Error',
+				description: 'Ocurrio un error al crear la cita.',
+			})
+		},
+	})
+}
