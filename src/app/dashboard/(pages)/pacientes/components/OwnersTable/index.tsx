b1fe0ea9213623b93e3adb642193b8717eb3951f
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-10-30 20:08:02 -0500

    feat(dashboard/patients): :sparkles: new index using tanstack/table

--- src/app/dashboard/(pages)/pacientes/components/OwnersTable/index.tsx
+++ src/app/dashboard/(pages)/pacientes/components/OwnersTable/index.tsx
@@ -1,68 +1,16 @@
-// Next
-import Link from 'next/link'
-
-// Utils
-import { cn } from '@/lib/utils'
-
-// Interfaces
-import { Owner } from '@/interfaces/Owner'
-
 // Components
 import { MaxWidthWrapper } from '@/components'
 
-// Shadcn Components
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-	buttonVariants,
-} from '@/components/shadcn'
-
 // Lucide Icons
-import { User } from 'lucide-react'
+import { DataTable } from './data-table'
+import { columns } from './columns'
+import useOwner from '@/hooks/useOwner'
 
-interface OwnersTableProps {
-	owners: Owner[]
-}
-
-export default function OwnersTable({ owners }: OwnersTableProps) {
+export default function OwnersTable() {
+	const { owners } = useOwner()
 	return (
 		<MaxWidthWrapper>
-			<Table>
-				<TableHeader>
-					<TableRow>
-						<TableHead>Nombre</TableHead>
-						<TableHead>DNI</TableHead>
-						<TableHead>Mascotas</TableHead>
-						<TableHead>Correo</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{owners.map(owner => (
-						<TableRow key={owner.id}>
-							<TableCell>{owner.name}</TableCell>
-							<TableCell>{owner?.dni}</TableCell>
-							<TableCell>{owner.pets && owner?.pets.length}</TableCell>
-							<TableCell>{owner?.email}</TableCell>
-							<TableCell>
-								<Link
-									href={`/dashboard/pacientes/${owner.id}`}
-									className={cn(
-										buttonVariants({ variant: 'outline' }),
-										'w-full'
-									)}
-								>
-									<User className='mr-2 size-4' />
-									Ver Detalles
-								</Link>
-							</TableCell>
-						</TableRow>
-					))}
-				</TableBody>
-			</Table>
+			<DataTable columns={columns} data={owners} />
 		</MaxWidthWrapper>
 	)
 }
