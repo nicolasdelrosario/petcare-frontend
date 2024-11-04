f7077dadf53463290298ba01d5b8c794d9355723
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-04 18:25:20 -0500

    refactor(global): :lipstick: update styles

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,10 +3,10 @@ import { Progress, Skeleton } from '../shadcn'
 
 export default function Loading() {
 	return (
-		<MaxWidthWrapper className='space-y-4 p-4'>
+		<MaxWidthWrapper className='p-4'>
 			<h1 className='text-2xl font-semibold'>Cargando...</h1>
-			<Progress value={60} className='mb-8 h-2 w-full' />
-			<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+			<Progress value={60} className='mt-16 h-2 w-full' />
+			<div className='mt-16 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 				{Array.from({ length: 12 }).map((_, index) => (
 					<div key={index} className='rounded-lg border p-4 shadow-sm'>
 						<Skeleton className='mb-2 h-6 w-1/2' />
