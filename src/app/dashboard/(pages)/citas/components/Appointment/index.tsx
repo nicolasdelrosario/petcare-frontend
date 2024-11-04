f7d7c004b953fdc9096a71bc6e82740c9d235b29
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-04 18:27:39 -0500

    refactor(dashboard/appointments): :recycle: refactor Appointment component

--- src/app/dashboard/(pages)/citas/components/Appointment/index.tsx
+++ src/app/dashboard/(pages)/citas/components/Appointment/index.tsx
@@ -13,33 +13,48 @@ interface AppointmentProps {
 
 export default function Appointment({ appointment }: AppointmentProps) {
 	const {
+		updatedAt,
 		pet: { name: petName, owner: { name: ownerName } = {} } = {},
 		date,
 		reason,
 	} = appointment
 
+	const appointmentDetails = [
+		{
+			icon: User,
+			label: ownerName,
+		},
+		{
+			icon: PawPrint,
+			label: petName,
+		},
+		{
+			icon: Calendar,
+			label: format(new Date(date), 'dd MMMM yyyy'),
+		},
+		{
+			icon: Clock,
+			label: format(new Date(date), 'HH:mm'),
+		},
+		{
+			icon: NotepadText,
+			label: reason || 'No hay notas',
+		},
+	]
+
 	return (
-		<>
-			<div className='flex items-center text-pretty pb-2 text-start text-muted-foreground'>
-				<User className='mr-2 size-4 min-w-4' />
-				{ownerName}
+		<div className='space-y-3'>
+			<div className='grid gap-3.5 text-muted-foreground'>
+				{appointmentDetails.map(({ icon: Icon, label }, index) => (
+					<div key={index} className='flex items-center'>
+						{Icon && <Icon className='mr-2 h-4 w-4' />}
+						{label}
+					</div>
+				))}
 			</div>
-			<div className='flex items-center text-pretty pb-2 text-start text-muted-foreground'>
-				<PawPrint className='mr-2 size-4 min-w-4' />
-				{petName}
+			<div className='flex items-center pt-2 text-sm text-muted-foreground'>
+				Última actualización: {format(new Date(updatedAt), 'dd MMM yyyy HH:mm')}
 			</div>
-			<div className='flex items-center text-pretty pb-2 text-start text-muted-foreground'>
-				<Calendar className='mr-2 size-4 min-w-4' />
-				{format(new Date(date), 'dd MMMM yyyy')}
-			</div>
-			<div className='flex items-center text-pretty pb-2 text-start text-muted-foreground'>
-				<Clock className='mr-2 size-4 min-w-4' />
-				{format(new Date(date), 'HH:mm')}
-			</div>
-			<div className='flex items-center text-pretty text-start text-muted-foreground'>
-				<NotepadText className='mr-2 size-4 min-w-4' />
-				{reason || 'No hay notas'}
-			</div>
-		</>
+		</div>
 	)
 }
