e3307ead5d1ff9a06273b82042ec3477d205dbc2
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-11-08 17:35:02 -0500

    feat(dashboard/patients): :sparkles: Add data validations with Zod before creating a owner

--- src/app/dashboard/components/AddOwnerForm/index.tsx
+++ src/app/dashboard/components/AddOwnerForm/index.tsx
@@ -3,9 +3,13 @@
 // Hooks
 import { useForm } from '@/hooks/useForm'
 import { useCreateOwner } from '@/hooks/owners/useCreateOwner'
+import { useState } from 'react'
 
 // Interfaces
 import { Owner } from '@/interfaces/Owner'
+
+//Schemas
+import { addOwnerSchema } from '../../(pages)/pacientes/[id]/components/OwnerSchema/addOwnerSchema'
 
 // Components
 import { AnimatedInput } from '@/components'
@@ -16,30 +20,48 @@ import { Button } from '@/components/shadcn'
 // Lucide Icons
 import { Mail, Phone, IdCard, MapPin } from 'lucide-react'
 
+//Zod
+import { z } from 'zod'
+
 interface AddOwnerFormProps {
 	onSuccess: () => void
 }
 
 export default function AddOwnerForm({ onSuccess }: AddOwnerFormProps) {
 	const createOwner = useCreateOwner()
-
 	const { formData: ownerData, handleChange } = useForm<Partial<Owner>>(
 		{} as Partial<Owner>
 	)
-
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		createOwner.mutate(ownerData)
-		onSuccess()
-	}
+	const [errors, setErrors] = useState<{ [key: string]: string }>({})
 
 	const ownerFields = [
 		{ id: 'name', label: 'Nombre', type: 'text' },
 		{ id: 'dni', label: 'DNI', type: 'text', icon: <IdCard /> },
-		{ id: 'email', label: 'Email', type: 'email', icon: <Mail /> },
+		{ id: 'email', label: 'Email', type: 'text', icon: <Mail /> },
 		{ id: 'phone', label: 'Teléfono', type: 'tel', icon: <Phone /> },
 		{ id: 'address', label: 'Dirección', type: 'text', icon: <MapPin /> },
 	]
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		try {
+			addOwnerSchema.parse(ownerData)
+			setErrors({})
+
+			createOwner.mutate(ownerData)
+			onSuccess()
+		} catch (error) {
+			if (error instanceof z.ZodError) {
+				const newErrors: { [key: string]: string } = {}
+				error.errors.forEach(err => {
+					if (err.path[0]) {
+						newErrors[err.path[0] as string] = err.message
+					}
+				})
+				setErrors(newErrors)
+			}
+		}
+	}
 
 	return (
 		<form onSubmit={handleSubmit}>
@@ -52,6 +74,7 @@ export default function AddOwnerForm({ onSuccess }: AddOwnerFormProps) {
 						type={field.type}
 						icon={field.icon}
 						onChange={handleChange}
+						error={errors[field.id] || ''}
 					/>
 				))}
 			</div>
