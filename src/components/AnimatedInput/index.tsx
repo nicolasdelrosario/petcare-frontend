c4e1e68bd7c858fd0e709dc77f02382c64ebd188
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-09 02:27:05 -0500

    feat(global): :sparkles: add className prop

--- src/components/AnimatedInput/index.tsx
+++ src/components/AnimatedInput/index.tsx
@@ -5,27 +5,29 @@ import { Input, Label } from '@/components/shadcn'
 import { cn } from '@/lib/utils'
 
 interface AnimatedInputProps {
+	className?: string
+	defaultValue?: string
+	error?: string
+	icon?: React.ReactNode
 	id: string
 	label: string
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+	placeholder?: string
 	type?: string
 	value?: string
-	defaultValue?: string
-	placeholder?: string
-	icon?: React.ReactNode
-	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-	error?: string
 }
 
-export default function AnimatedLabelInput({
+export default function AnimatedInput({
+	className,
+	defaultValue,
+	error,
+	icon,
 	id,
 	label,
+	onChange,
+	placeholder = '',
 	type = 'text',
 	value,
-	defaultValue,
-	placeholder = '',
-	icon,
-	onChange,
-	error,
 }: AnimatedInputProps) {
 	return (
 		<div className='group relative my-4'>
@@ -33,7 +35,7 @@ export default function AnimatedLabelInput({
 				htmlFor={id}
 				className='origin-start absolute top-1/2 z-10 block -translate-y-1/2 cursor-text px-1 text-xs text-muted-foreground/70 transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:-translate-y-1/2 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium group-focus-within:text-foreground has-[+div>input:not(:placeholder-shown)]:pointer-events-none has-[+div>input:not(:placeholder-shown)]:top-0 has-[+div>input:not(:placeholder-shown)]:-translate-y-1/2 has-[+div>input:not(:placeholder-shown)]:cursor-default has-[+div>input:not(:placeholder-shown)]:text-xs has-[+div>input:not(:placeholder-shown)]:font-medium has-[+div>input:not(:placeholder-shown)]:text-foreground'
 			>
-				<span className='inline-flex bg-background px-2'>{label}</span>
+				<span className='inline-flex bg-transparent px-2'>{label}</span>
 			</Label>
 
 			<div className='relative flex items-center'>
@@ -44,7 +46,11 @@ export default function AnimatedLabelInput({
 					value={value}
 					defaultValue={defaultValue}
 					placeholder={placeholder}
-					className={cn('text-pretty pr-10 text-base', icon && 'pr-10')}
+					className={cn(
+						'text-pretty pr-10 text-base',
+						icon && 'pr-10',
+						className
+					)}
 					onChange={onChange}
 				/>
 				{icon && (
