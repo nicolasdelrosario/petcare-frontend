f0e623aeed405be8ff1e165f3cd56fe7cdc91b54
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-10 16:32:54 -0500

    feat(dashboard/appointments): :sparkles: create AddAppointmentForm functionality

--- src/app/dashboard/components/AddAppointmentForm/index.tsx
+++ src/app/dashboard/components/AddAppointmentForm/index.tsx
@@ -2,37 +2,100 @@
 
 // Hooks
 import { useForm } from '@/hooks/useForm'
-import { useOwners } from '@/hooks/owners/useOwners'
+import { usePets } from '@/hooks/pets/usePets'
 import { useCreateAppointment } from '@/hooks/appointments/useCreateAppointment'
 
 // Interfaces
 import { Appointment } from '@/interfaces/Appointment'
+import { Pet } from '@/interfaces/Pet'
+
+// Components
+import { AnimatedInput, AnimatedSelect } from '@/components'
+
+// Shadcn Components
+import { Button } from '@/components/shadcn'
+
+// Lucide Icons
+import { Clock, NotepadText } from 'lucide-react'
 
 interface AddAppointmentFormProps {
 	onSuccess: () => void
 }
 
+type AppointmentFormData = Partial<Appointment> & {
+	petId: number
+	userId: number
+}
+
 export default function AddAppointmentForm({
 	onSuccess,
 }: AddAppointmentFormProps) {
-	// const { data: owners } = useOwners()
-	// const createAppointment = useCreateAppointment()
+	const pets = usePets()
+	const createAppointment = useCreateAppointment()
 
-	// const {
-	// 	formData: appointmentData,
-	// 	handleChange,
-	// 	setFormData,
-	// } = useForm<Partial<Appointment>>({} as Partial<Appointment>)
+	const {
+		formData: appointmentData,
+		handleChange,
+		updateField,
+	} = useForm<Partial<AppointmentFormData>>({
+		userId: 1,
+	} as Partial<AppointmentFormData>)
 
-	// const handleSelectOwner = (ownerId: string) => {
-	// 	setFormData(prev => ({ ...prev, ownerId: Number(ownerId) }))
-	// }
+	const handleSelectPet = (petId: string) => {
+		updateField('petId', Number(petId))
+	}
 
-	// const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-	// 	e.preventDefault()
-	// 	createAppointment.mutate(appointmentData)
-	// 	onSuccess()
-	// }
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		createAppointment.mutate(appointmentData)
+		onSuccess()
+	}
 
-	return <div>index</div>
+	return (
+		<form onSubmit={handleSubmit}>
+			<AnimatedSelect
+				id='pet'
+				label='Mascota'
+				options={
+					pets.data?.map((pet: Pet) => ({
+						id: pet.id,
+						name: pet.name,
+					})) ?? []
+				}
+				value={appointmentData.petId?.toString()}
+				onChange={handleSelectPet}
+			/>
+
+			<div className='grid grid-cols-2 gap-4'>
+				<AnimatedInput
+					key='date'
+					id='date'
+					label='Fecha'
+					type='date'
+					onChange={handleChange}
+				/>
+				<AnimatedInput
+					key='time'
+					id='time'
+					label='Hora'
+					type='time'
+					onChange={handleChange}
+					icon={<Clock />}
+				/>
+			</div>
+
+			<AnimatedInput
+				key='reason'
+				id='reason'
+				label='Motivo'
+				type='text'
+				onChange={handleChange}
+				icon={<NotepadText />}
+			/>
+
+			<div className='mt-8 flex justify-end'>
+				<Button type='submit'>Agregar Propietario</Button>
+			</div>
+		</form>
+	)
 }
