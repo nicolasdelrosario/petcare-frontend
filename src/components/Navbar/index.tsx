59dd70b170c791edb70b408579f44295690f4609
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-13 15:46:13 -0500

    feat(global): :sparkles: update navbar, now getting the session to see if someone is logged or not

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,23 @@
+'use client'
+
 // Next
 import Link from 'next/link'
+
+// Auth
+import { useSession, signOut } from 'next-auth/react'
 
 // Components
 import { MaxWidthWrapper } from '@/components'
 
 // Shadcn
-import { buttonVariants } from '../shadcn/button'
+import { buttonVariants, Button } from '../shadcn/button'
 
 // Icons
 import { ArrowRightIcon } from '@radix-ui/react-icons'
 
 export default function Navbar() {
+	const { data: session } = useSession()
+
 	return (
 		<MaxWidthWrapper>
 			<nav className='flex h-14 items-center justify-between border-b border-zinc-200'>
@@ -18,29 +25,55 @@ export default function Navbar() {
 					Petcare
 				</Link>
 				<div className='flex h-full items-center space-x-4'>
-					<Link
-						href='/'
-						className={buttonVariants({
-							size: 'sm',
-							className: 'text-xs sm:text-sm',
-						})}
-					>
-						Registrar
-						<ArrowRightIcon className='ml-1.5 h-5 w-5' />
-					</Link>
+					{session?.user ? (
+						<>
+							<Link
+								href='/dashboard/home'
+								className={buttonVariants({
+									size: 'sm',
+									className: 'text-xs sm:text-sm',
+								})}
+							>
+								Dashboard
+							</Link>
 
-					<div className='hidden h-8 w-px bg-zinc-200 sm:block' />
+							<div className='hidden h-8 w-px bg-zinc-200 sm:block' />
 
-					<Link
-						href='/iniciar-sesion'
-						className={buttonVariants({
-							size: 'sm',
-							variant: 'outline',
-							className: 'text-xs sm:text-sm',
-						})}
-					>
-						Login
-					</Link>
+							<Button
+								variant='outline'
+								className='text-xs sm:text-sm'
+								onClick={() => signOut()}
+							>
+								Logout
+							</Button>
+						</>
+					) : (
+						<>
+							<Link
+								href='/'
+								className={buttonVariants({
+									size: 'sm',
+									className: 'text-xs sm:text-sm',
+								})}
+							>
+								Registrar
+								<ArrowRightIcon className='ml-1.5 h-5 w-5' />
+							</Link>
+
+							<div className='hidden h-8 w-px bg-zinc-200 sm:block' />
+
+							<Link
+								href='/iniciar-sesion'
+								className={buttonVariants({
+									size: 'sm',
+									variant: 'outline',
+									className: 'text-xs sm:text-sm',
+								})}
+							>
+								Login
+							</Link>
+						</>
+					)}
 				</div>
 			</nav>
 		</MaxWidthWrapper>
