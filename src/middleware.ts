5734edbd01632016baf6b33b9bb672de4b2e6df9
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-13 19:36:49 -0500

    fix(global): :bug: there is a validation now if the user is logged or not

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,20 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-	return NextResponse.redirect(new URL('/', request.url))
+export async function middleware(request: NextRequest) {
+	// Retrieve token from NextAuth's JWT
+	const token = await getToken({
+		req: request,
+		secret: process.env.NEXTAUTH_SECRET,
+	})
+
+	// If there is no token (user is not authenticated), redirect to the homepage
+	if (!token) return NextResponse.redirect(new URL('/', request.url))
 }
 
-// See "Matching Paths" below to learn more
+// Match paths starting with /dashboard
 export const config = {
 	matcher: '/dashboard/:path*',
 }
