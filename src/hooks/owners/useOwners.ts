49af4d68804ca35c2caf873ee67c5cd99f547335
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-14 19:16:07 -0500

    refactor(hooks): :recycle: update token handling in useOwners hook

--- src/hooks/owners/useOwners.ts
+++ src/hooks/owners/useOwners.ts
@@ -1,10 +1,21 @@
+// Tankstack Query
 import { useQuery } from '@tanstack/react-query'
-import { getOwners } from '@/services/owner.service'
+
+// Services
+import { ownerService } from '@/services/owner.service'
+
+// Hooks
+import { useToken } from '@/services/auth'
 
 export const useOwners = () => {
+	const token = useToken()
+
 	return useQuery({
 		queryKey: ['owners'],
-		queryFn: getOwners,
+		queryFn: () => {
+			if (!token) throw new Error('No token found')
+			return ownerService.getOwners(token)
+		},
 		staleTime: 1000 * 60 * 5,
 		refetchOnWindowFocus: true,
 	})
