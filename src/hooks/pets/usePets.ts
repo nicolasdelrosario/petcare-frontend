e1a63185a5f29a7834e927d27a47fb1b05128f70
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-14 19:06:05 -0500

    refactor(hooks): :recycle: update token handling in usePets hook

--- src/hooks/pets/usePets.ts
+++ src/hooks/pets/usePets.ts
@@ -1,10 +1,21 @@
+// Tankstack Query
 import { useQuery } from '@tanstack/react-query'
-import { getPets } from '@/services/pet.service'
+
+// Services
+import { petService } from '@/services/pet.service'
+
+// Hooks
+import { useToken } from '@/services/auth'
 
 export const usePets = () => {
+	const token = useToken()
+
 	return useQuery({
 		queryKey: ['pets'],
-		queryFn: getPets,
+		queryFn: () => {
+			if (!token) throw new Error('No token found')
+			return petService.getPets(token)
+		},
 		staleTime: 1000 * 60 * 5,
 		refetchOnWindowFocus: true,
 	})
