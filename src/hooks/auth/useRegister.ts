97a82b033a8843042f39ac00d951957b668ffef3
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-14 19:26:18 -0500

    feat(auth): :passport_control: add useRegister hook for handling user registration

---
+++ src/hooks/auth/useRegister.ts
@@ -0,0 +1,38 @@
+// Tankstack Query
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+// Services
+import { authService } from '@/services/auth.service'
+
+// Interfaces
+import { RegisterCredentials } from '@/interfaces/auth'
+
+// Hooks
+import { useToast } from '@/hooks/useToast'
+
+export const useRegister = () => {
+	const queryClient = useQueryClient()
+	const { toast } = useToast()
+
+	return useMutation({
+		mutationFn: (credentials: RegisterCredentials) => {
+			return authService.register(credentials)
+		},
+
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['user'] })
+			toast({
+				title: 'Éxito',
+				description: 'El usuario ha sido creado exitosamente.',
+			})
+		},
+
+		onError: () => {
+			toast({
+				variant: 'destructive',
+				title: 'Error',
+				description: 'No ha sido posible crear el usuario.',
+			})
+		},
+	})
+}
