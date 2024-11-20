ff120d6c7425644a33723a0fa4558e3a18c18154
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-11-20 13:11:35 -0500

    feat(appointments): :sparkles: add AppointmentSchema that include create and update validations

---
+++ src/app/dashboard/(pages)/citas/components/AppointmentSchema/addAppointmentSchema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const addAppointmentSchema = z.object({
+	date: z.date().refine(date => date.getTime() >= Date.now(), {
+		message: 'La fecha de la cita no puede ser menor a la fecha actual',
+	}),
+	time: z.string({ required_error: 'La hora es requerida' }),
+	reason: z
+		.string({ required_error: 'El motivo de la cita es obligatorio' })
+		.min(3, { message: 'El motivo debe tener al menos 3 caracteres' }),
+	petId: z.number(),
+	userId: z.number(),
+})
