d21892eb2f5c994cdddeb359113e0660d8d0f073
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-11-28 16:58:38 -0500

    style(dashboard): :lipstick: Add dark class to the title

--- src/app/dashboard/(pages)/home/page.tsx
+++ src/app/dashboard/(pages)/home/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
 	return (
 		<div className='grid min-h-[100vh] place-content-center'>
-			<h1 className='mt-16 w-fit text-balance text-4xl font-bold leading-relaxed tracking-tight text-gray-900 md:text-5xl md:!leading-tight lg:text-6xl'>
+			<h1 className='mt-16 w-fit text-balance text-4xl font-bold leading-relaxed tracking-tight text-gray-900 dark:text-white md:text-5xl md:!leading-tight lg:text-6xl'>
 				Hello, welcome back!
 			</h1>
 			<p className='mt-6 max-w-prose text-balance text-center text-lg md:text-wrap lg:pr-10'>
