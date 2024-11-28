205b21266a67a5340660a2257b28c8a4a27b5ed1
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-11-28 16:57:20 -0500

    feat(global): :sparkles: Add ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import { cn } from '@/lib/utils'
 
 // Components
 import { Navbar, Footer, Providers } from '@/components'
+import { ThemeProvider } from '@/components/Providers/ThemeProvider'
 
 const poppins = Poppins({
 	subsets: ['latin'],
@@ -30,21 +31,28 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	const isHomePage = pathname === '/'
 
 	return (
-		<html lang='es'>
+		<html lang='es' suppressHydrationWarning>
 			<body className={cn('min-h-screen', poppins.className)}>
-				<Providers>
-					{isHomePage && (
-						<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
-							<Navbar />
-						</header>
-					)}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Providers>
+						{isHomePage && (
+							<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+								<Navbar />
+							</header>
+						)}
 
-					<main className='grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col'>
-						<div className='flex h-full flex-1 flex-col'>{children}</div>
-					</main>
+						<main className='grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col'>
+							<div className='flex h-full flex-1 flex-col'>{children}</div>
+						</main>
 
-					{isHomePage && <Footer />}
-				</Providers>
+						{isHomePage && <Footer />}
+					</Providers>
+				</ThemeProvider>
 			</body>
 		</html>
 	)
