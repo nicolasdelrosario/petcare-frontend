f491af845ad419d818395c88981e97206b1443ea
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-29 21:45:29 -0500

    refactor(global): :fire: remove ThemeProvider from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ import { cn } from '@/lib/utils'
 
 // Components
 import { Navbar, Footer, Providers } from '@/components'
-import { ThemeProvider } from '@/components/Providers/ThemeProvider'
 
 const poppins = Poppins({
 	subsets: ['latin'],
@@ -33,26 +32,19 @@ export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang='es' suppressHydrationWarning>
 			<body className={cn('min-h-screen', poppins.className)}>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='system'
-					enableSystem
-					disableTransitionOnChange
-				>
-					<Providers>
-						{isHomePage && (
-							<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
-								<Navbar />
-							</header>
-						)}
+				<Providers>
+					{isHomePage && (
+						<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+							<Navbar />
+						</header>
+					)}
 
-						<main className='grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col'>
-							<div className='flex h-full flex-1 flex-col'>{children}</div>
-						</main>
+					<main className='grainy-light flex min-h-[calc(100vh-3.5rem-1px)] flex-col'>
+						<div className='flex h-full flex-1 flex-col'>{children}</div>
+					</main>
 
-						{isHomePage && <Footer />}
-					</Providers>
-				</ThemeProvider>
+					{isHomePage && <Footer />}
+				</Providers>
 			</body>
 		</html>
 	)
