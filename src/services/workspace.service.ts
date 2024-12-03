f678fd15a5fe93804550fb63091181dbc88d081d
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-03 16:50:43 -0500

    feat(services): :sparkles: add workspace services

--- src/services/workspace.service.ts
+++ src/services/workspace.service.ts
@@ -7,18 +7,15 @@ import { Workspace } from '@/interfaces/Workspace'
 const PATH_WORKSPACES = '/workspaces'
 
 export const workspaceService = {
-	getWorkspaceById: async (id: number, token: string): Promise<Workspace> => {
-		const api = createApiInstance(token)
+	getWorkspaceById: async (id: number): Promise<Workspace> => {
+		const api = createApiInstance()
 		const { data } = await api.get(`${PATH_WORKSPACES}/${id}`)
 
 		return data
 	},
 
-	createWorkspace: async (
-		changes: Partial<Workspace>,
-		token: string
-	): Promise<Workspace> => {
-		const api = createApiInstance(token)
+	createWorkspace: async (changes: Partial<Workspace>): Promise<Workspace> => {
+		const api = createApiInstance()
 		const { data } = await api.post(PATH_WORKSPACES, changes)
 
 		return data
