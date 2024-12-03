1ca1200536758c0123da224913d0943db41244ef
GeremyOcsa <geremy.ocsa11@gmail.com>
2024-12-02 20:52:48 -0500

    style(global): :lipstick: Fix dark mode

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,11 +4,11 @@ import { MaxWidthWrapper } from '@/components'
 
 export default function Footer() {
 	return (
-		<footer className='relative h-20 border-t border-gray-200 bg-white'>
+		<footer className='relative h-20 border-t border-gray-200 bg-white dark:border-gray-400 dark:bg-black'>
 			<MaxWidthWrapper>
 				<div className='flex h-full flex-col items-center justify-center lg:flex-row lg:justify-between'>
 					<div className='pb-4 text-center lg:pb-0 lg:text-left'>
-						<p className='text-sm text-muted-foreground'>
+						<p className='text-sm text-muted-foreground dark:text-gray-100'>
 							&copy; {new Date().getFullYear()} Todos los derechos reservados.
 						</p>
 					</div>
@@ -17,13 +17,13 @@ export default function Footer() {
 						<div className='flex space-x-8'>
 							<Link
 								href='/terminos-condiciones'
-								className='text-sm text-muted-foreground hover:text-gray-600'
+								className='text-sm text-muted-foreground hover:text-gray-600 dark:hover:text-gray-100'
 							>
 								Términos y condiciones
 							</Link>
 							<Link
 								href='/'
-								className='text-sm text-muted-foreground hover:text-gray-600'
+								className='text-sm text-muted-foreground hover:text-gray-600 dark:hover:text-gray-100'
 							>
 								Política de Privacidad
 							</Link>
