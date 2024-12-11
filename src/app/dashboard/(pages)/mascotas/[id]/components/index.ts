7d7f2df5e88a18feee64dc77ee99117497b3c662
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-11 18:55:41 -0500

    chore: :technologist: update exportable file

--- src/app/dashboard/(pages)/mascotas/[id]/components/index.ts
+++ src/app/dashboard/(pages)/mascotas/[id]/components/index.ts
@@ -1,2 +1,4 @@
 export { default as PetDetails } from './PetDetails'
 export { default as PetHistory } from './PetHistory'
+export { default as PetHistoryPDF } from './PetHistoryPDF'
+export { default as PDFGeneratorService } from './PDFGeneratorService'
