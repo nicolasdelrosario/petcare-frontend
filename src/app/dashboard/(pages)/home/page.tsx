6488b8bafef78c2889e6b1baa9530eb2efd86c7b
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-15 18:20:57 -0500

    feat(dashboard): :sparkles: now i'm getting their actual name of the user, also, i've changed it to spanish the text

--- src/app/dashboard/(pages)/home/page.tsx
+++ src/app/dashboard/(pages)/home/page.tsx
@@ -1,11 +1,18 @@
+'use client'
+
+// Auth
+import { useSession } from 'next-auth/react'
+
 export default function Page() {
+	const { data: session } = useSession()
+
 	return (
-		<div className='grid min-h-[100vh] place-content-center'>
+		<div className='grid min-h-[100vh] place-content-center text-center'>
 			<h1 className='mt-16 w-fit text-balance text-4xl font-bold leading-relaxed tracking-tight text-gray-900 dark:text-white md:text-5xl md:!leading-tight lg:text-6xl'>
-				Hello, welcome back!
+				Hola, {session?.user?.name}!
 			</h1>
-			<p className='mt-6 max-w-prose text-balance text-center text-lg md:text-wrap lg:pr-10'>
-				Are you looking for something?
+			<p className='mt-6 max-w-prose text-balance text-xl md:text-wrap lg:pr-10'>
+				¿Que deseas hacer hoy?
 			</p>
 		</div>
 	)
