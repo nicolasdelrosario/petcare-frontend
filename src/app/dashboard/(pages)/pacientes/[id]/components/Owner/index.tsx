cc2599489d9dc42e874d69958ff73d4dad7dfcf0
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-15 14:32:09 -0500

    style: :lipstick: update ui for a better responsive

--- src/app/dashboard/(pages)/pacientes/[id]/components/Owner/index.tsx
+++ src/app/dashboard/(pages)/pacientes/[id]/components/Owner/index.tsx
@@ -15,18 +15,18 @@ export default function Owner({ owner }: OwnerProps) {
 	const ownerDetails = generateOwnerDetails(owner)
 
 	return (
-		<div>
+		<div className='w-full max-w-md sm:max-w-lg'>
 			{ownerDetails.map(({ icon: Icon, value }, index) => (
 				<div
 					key={index}
 					className={`flex items-center ${index > 0 ? 'mt-3' : ''}`}
 				>
 					<Icon className='mr-2 size-4' />
-					<span>{value}</span>
+					<span className='truncate tracking-tight'>{value}</span>
 				</div>
 			))}
 
-			<div className='flex items-center pt-4 text-sm text-muted-foreground'>
+			<div className='flex items-center truncate pt-4 text-sm tracking-tight text-muted-foreground'>
 				Última actualización:{' '}
 				{format(new Date(owner.updatedAt), 'dd MMM yyyy HH:mm')}
 			</div>
