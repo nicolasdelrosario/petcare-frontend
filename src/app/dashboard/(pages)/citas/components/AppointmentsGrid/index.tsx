c51f107743220ae082179f3d084b1c4b1a88bf00
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-15 18:52:00 -0500

    feat(dashboard): :sparkles: add some new validations

--- src/app/dashboard/(pages)/citas/components/AppointmentsGrid/index.tsx
+++ src/app/dashboard/(pages)/citas/components/AppointmentsGrid/index.tsx
@@ -32,33 +32,35 @@ export default function AppointmentsGrid({
 			<MaxWidthWrapper>
 				<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 					{appointments.map(appointment => {
-						const { id, pet, dateTime } = appointment
-						const { name: petName, owner } = pet
-						const { name: ownerName } = owner
+						if (appointment !== null && appointment.pet !== null) {
+							const { id, pet, dateTime } = appointment
+							const { name: petName, owner } = pet
+							const { name: ownerName } = owner
 
-						return (
-							<Card key={id}>
-								<CardHeader className='border-b p-4'>
-									<CardTitle className='truncate text-sm font-semibold'>
-										{ownerName}
-									</CardTitle>
-								</CardHeader>
-								<CardContent className='p-4'>
-									<p className='text-sm font-medium'>{petName}</p>
-									<div className='flex items-center text-sm text-muted-foreground'>
-										<Calendar className='mr-2 size-4' />
-										{dateTime && formatDate(dateTime, 'dd MMM yyyy')}
-									</div>
-									<div className='flex items-center text-sm text-muted-foreground'>
-										<Clock className='mr-2 size-4' />
-										{dateTime && formatDate(dateTime, 'HH:mm')}
-									</div>
-								</CardContent>
-								<CardFooter className='px-4 pb-4'>
-									<AppointmentDetailsDialog appointment={appointment} />
-								</CardFooter>
-							</Card>
-						)
+							return (
+								<Card key={id}>
+									<CardHeader className='border-b p-4'>
+										<CardTitle className='truncate text-sm font-semibold'>
+											{ownerName}
+										</CardTitle>
+									</CardHeader>
+									<CardContent className='p-4'>
+										<p className='text-sm font-medium'>{petName}</p>
+										<div className='flex items-center text-sm text-muted-foreground'>
+											<Calendar className='mr-2 size-4' />
+											{dateTime && formatDate(dateTime, 'dd MMM yyyy')}
+										</div>
+										<div className='flex items-center text-sm text-muted-foreground'>
+											<Clock className='mr-2 size-4' />
+											{dateTime && formatDate(dateTime, 'HH:mm')}
+										</div>
+									</CardContent>
+									<CardFooter className='px-4 pb-4'>
+										<AppointmentDetailsDialog appointment={appointment} />
+									</CardFooter>
+								</Card>
+							)
+						}
 					})}
 				</div>
 			</MaxWidthWrapper>
