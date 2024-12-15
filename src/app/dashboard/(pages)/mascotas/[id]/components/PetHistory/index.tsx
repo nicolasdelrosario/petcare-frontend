7b0cbc11dfbe4bd5671940092f95caf9921b32ef
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-15 14:33:13 -0500

    feat: :sparkles: add new validations to show in case there is no history

--- src/app/dashboard/(pages)/mascotas/[id]/components/PetHistory/index.tsx
+++ src/app/dashboard/(pages)/mascotas/[id]/components/PetHistory/index.tsx
@@ -21,39 +21,48 @@ export default function PetHistory({ pet }: PetHistoryProps) {
 	return (
 		<ScrollArea className='h-[calc(100vh-12rem)] pr-4'>
 			<div className='flex flex-col gap-y-4 space-y-4'>
-				{pet.appointments.map(appointment => {
-					const { dateTime, reason } = appointment
+				{pet.appointments.length > 0 ? (
+					pet.appointments.map(appointment => {
+						const { dateTime, reason } = appointment
 
-					return (
-						<Card key={appointment.id}>
-							<CardContent className='p-4'>
-								<div className='flex items-start justify-between'>
-									<div className='space-y-1'>
-										<div className='flex items-center space-x-2 font-medium'>
-											<Calendar className='size-5' />
-											<span className='text-md truncate tracking-tight'>
-												{dateTime
-													? formatDate(dateTime, 'dd MMM yyyy')
-													: 'Fecha no disponible'}
-											</span>
-										</div>
-										<div className='flex items-center space-x-2 font-medium text-muted-foreground'>
-											<NotepadText className='size-5' />
-											<span className='text-md'>
-												{reason || 'Motivo no especificada'}
-											</span>
+						return (
+							<Card key={appointment.id}>
+								<CardContent className='p-4'>
+									<div className='flex items-start justify-between'>
+										<div className='space-y-1'>
+											<div className='flex items-center space-x-2 font-medium'>
+												<Calendar className='size-5' />
+												<span className='text-md truncate tracking-tight'>
+													{dateTime
+														? formatDate(dateTime, 'dd MMM yyyy')
+														: 'Fecha no disponible'}
+												</span>
+											</div>
+											<div className='flex items-center space-x-2 font-medium text-muted-foreground'>
+												<NotepadText className='size-5' />
+												<span className='text-md'>
+													{reason || 'Motivo no especificado'}
+												</span>
+											</div>
 										</div>
 									</div>
-								</div>
-							</CardContent>
-						</Card>
-					)
-				})}
+								</CardContent>
+							</Card>
+						)
+					})
+				) : (
+					// Mensaje cuando no hay citas
+					<div className='mt-8 text-center text-muted-foreground'>
+						Todavía no se ha creado ninguna cita.
+					</div>
+				)}
 			</div>
 
-			<div className='mt-8 flex justify-end'>
-				<PDFGeneratorService pet={pet} />
-			</div>
+			{pet.appointments.length > 0 && (
+				<div className='mt-8 flex justify-end'>
+					<PDFGeneratorService pet={pet} />
+				</div>
+			)}
 		</ScrollArea>
 	)
 }
